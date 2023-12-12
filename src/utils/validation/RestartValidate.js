b7ed6b5b305ebce426708348f5eb36cbc0a9301d
skdbsqls <skdbsqls98@naver.com>
2023-12-12 14:13:00 +0900

    fix: 재시작 여부 입력값 예외 처리 수정

--- src/utils/validation/RestartValidate.js
+++ src/utils/validation/RestartValidate.js
@@ -1,9 +1,10 @@
 import { ERROR_MESSAGE } from "../../constants/message.js";
 
 const RestartValidate = {
+  // 입력값이 1 또는 2가 아닌 경우
   notRestart(restart) {
-    if (restart !== "1" || restart !== "2") {
-      throw new Error(ERROR_MESSAGE.restart);
+    if (restart !== "1" && restart !== "2") {
+      throw new Error(ERROR_MESSAGE.invalidRestart);
     }
   },
 };
