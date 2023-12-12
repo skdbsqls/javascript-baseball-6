009e25165be2660b6395618b22cb5a4548af53a2
skdbsqls <skdbsqls98@naver.com>
2023-12-12 13:24:53 +0900

    feat: 사용자 입력값에 대한 예외 처리하기

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,18 +1,42 @@
 import { Console } from "@woowacourse/mission-utils";
 import { INPUT_MESSAGE } from "../constants/message.js";
+import NumberValidate from "../utils/validation/NumberValidate.js";
+import RestartValidate from "../utils/validation/ReStartValidate.js";
 
 const InputView = {
   // 사용자 입력값 입력
   async readNumber() {
     const number = await Console.readLineAsync(INPUT_MESSAGE.number);
-
-    return number;
+    try {
+      this.validateNumber(number);
+      return number;
+    } catch (error) {
+      Console.print(error.message);
+    }
   },
+  // 사용자 입력값 예외 처리
+  validateNumber(number) {
+    NumberValidate.doNotEnter(number);
+    NumberValidate.IncludeSpace(number);
+    NumberValidate.isNotNum(number);
+    NumberValidate.invalidLength(number);
+    NumberValidate.invalidZero(number);
+    NumberValidate.duplicateNum(number);
+  },
+
   // 게임 재시작 여부 입력
   async readRestart() {
     const restart = await Console.readLineAsync(INPUT_MESSAGE.restart);
-
-    return restart;
+    try {
+      this.validateRestart(restart);
+      return restart;
+    } catch (error) {
+      Console.print(error.message);
+    }
+  },
+  // 게임 재시작 여부 예외 처리
+  validateRestart(restart) {
+    RestartValidate(restart);
   },
 };
 
