cbc4c193388dc347196c50972432304e89a98bb7
skdbsqls <skdbsqls98@naver.com>
2023-12-12 13:02:32 +0900

    feat: 입력 관련 함수 작성

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,3 +1,19 @@
-const InputView = {};
+import { Console } from "@woowacourse/mission-utils";
+import { INPUT_MESSAGE } from "../constants/message.js";
+
+const InputView = {
+  // 사용자 입력값 입력
+  async readNumber() {
+    const number = await Console.readLineAsync(INPUT_MESSAGE.number);
+
+    return number;
+  },
+  // 게임 재시작 여부 입력
+  async readRestart() {
+    const restart = await Console.readLineAsync(INPUT_MESSAGE.restart);
+
+    return restart;
+  },
+};
 
 export default InputView;
